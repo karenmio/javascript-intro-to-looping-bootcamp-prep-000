6b204614085ffc33f0294c178aa5f5e88f5a34a8
karen wang <karenkarenmio@gmail.com>
2018-06-18 21:07:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,25 @@
-const array=[]
+const array = [];
 
-for (var i=0; i<25; i++){
+  function forLoop() {
+    let i=0;
+    for (i=0; i<25; i++){
   if (i=1){
     array.push("I am 1 stange loop.")
-  } else {
+ } else {
     array.push('I am ${i} strange loops.')
+ }
+}
+
+
+return array;
+
+}
+ 
+ 
+function whileLoop() {
+  let countdown=100;
+  while (countdown>0) {
+    console.log(--countdown)
   }
+  return 'done';
 }
